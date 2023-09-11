608737b68df31bc6968f642cd8d422970e76c79c
teriTheDesigner <tere1109@stud.kea.dk>
2023-09-11 15:47:19 +0200

    changing data structure

--- pages/step1.vue
+++ pages/step1.vue
@@ -5,10 +5,14 @@ const globalStore = useGlobalStore();
 
 const displayPage = ref(false);
 
-const missionName = ref('');
-const missionDescription = ref('');
-const date = ref('');
 
+function changeReportName(e){
+    globalStore.setReportName( e.target.value);
+}
+
+function changeReportDescription(e){
+    globalStore.setReportDescription( e.target.value);
+}
 
 onMounted(()=>{
     const user = localStorage.getItem('validUser');
@@ -23,11 +27,11 @@ onMounted(()=>{
     <Stepper/>
     <label>
         Mission Name
-        <input v-model="missionName" class="bg-gray-200" type="text">
+        <input @input="changeReportName" class="bg-gray-200" type="text">
     </label>
     <label>
         Mission Description
-        <input v-model="missionDescription" class="bg-gray-200" type="text">
+        <input @input="changeReportDescription" class="bg-gray-200" type="text">
     </label>
 
  
--- store/user.js
+++ store/user.js
@@ -8,18 +8,17 @@ export const useGlobalStore = defineStore("user", {
     firstName: "",
     lastName: "",
     avatar: "",
-    reports: [
-      {
-        id: "",
-        name: "",
-        description: "",
-        date: "",
-        images: "",
-        longitude: "",
-        latitude: "",
-        timestamp: "",
-      },
-    ],
+    allReports: [],
+    report: {
+      id: null,
+      name: "",
+      description: "",
+      date: "",
+      images: [],
+      longitude: "",
+      latitude: "",
+      timestamp: "",
+    },
   }),
   actions: {
     setEmail(email) {
@@ -39,6 +38,41 @@ export const useGlobalStore = defineStore("user", {
     },
     setAvatar(avatar) {
       this.avatar = avatar;
+    },
+    setReportName(name) {
+      console.log(name);
+      this.report.name = name;
+    },
+    setReportDescription(description) {
+      this.report.description = description;
+    },
+
+    addReport() {
+      const newReportId = this.allReports.length + 1;
+
+      const newReport = {
+        id: newReportId,
+        name: this.report.name,
+        description: this.report.description,
+        date: this.report.date,
+        images: this.report.images,
+        longitude: this.report.longitude,
+        latitude: this.report.latitude,
+        timestamp: this.report.timestamp,
+      };
+
+      this.allReports.push(newReport);
+
+      this.report = {
+        id: null,
+        name: "",
+        description: "",
+        date: "",
+        images: [],
+        longitude: "",
+        latitude: "",
+        timestamp: "",
+      };
     },
   },
 });
