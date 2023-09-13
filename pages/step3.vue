f4a01ffb48693b06957c60e731e2f7e93a0b652b
teriTheDesigner <tere1109@stud.kea.dk>
2023-09-13 14:24:13 +0200

    map updates

--- pages/step3.vue
+++ pages/step3.vue
@@ -1,46 +1,30 @@
 <script setup>
-// useHead({
-//   script: [
-//     {
-//       src: "https://cdn.jsdelivr.net/npm/ol@v7.2.2/dist/ol.js",
-//       type: "text/javascript",
-//       async: true,
-//     },
-//   ],
-
-//   link: [
-//     {
-//       href: "https://cdn.jsdelivr.net/npm/ol@v7.2.2/ol.css",
-//       rel: "stylesheet",
-//     },
-//   ],
-// });
-
 import { ref, onMounted, watchEffect } from "vue";
-import { useGlobalStore } from "../store/user";
-const globalStore = useGlobalStore();
 
 const displayPage = ref(false);
 const latitude = ref(null);
 const longitude = ref(null);
+let currentTimestamp = ref(null);
 const map = ref(null);
-// const apiKey = process.env.VUE_APP_MAP_API_KEY;
+const markerLayer = ref(null);
 
 onMounted(async () => {
   const user = localStorage.getItem("validUser");
   displayPage.value = user && user !== "undefined";
 
   if (displayPage.value) {
-    fetchISSLocation();
-    setInterval(fetchISSLocation, 120000);
     await loadOpenLayersLibrary();
+    createMap();
+    fetchISSLocation();
+    setInterval(fetchISSLocation, 10000);
   }
 });
 
 const fetchISSLocation = async () => {
   const timestamp = Date.now();
   const unixTimestamp = Math.floor(timestamp / 3000);
-  console.log(unixTimestamp);
+  currentTimestamp = unixTimestamp;
+  console.log(currentTimestamp);
   try {
     const response = await fetch(
       `https://api.wheretheiss.at/v1/satellites/25544?timestamp=${unixTimestamp}`
@@ -49,6 +33,9 @@ const fetchISSLocation = async () => {
       const data = await response.json();
       latitude.value = data.latitude;
       longitude.value = data.longitude;
+
+      // Update the map with new coordinates
+      updateMap();
     } else {
       console.error("Failed to fetch ISS location data");
     }
@@ -71,58 +58,57 @@ const loadOpenLayersLibrary = () => {
   });
 };
 
-watchEffect(() => {
-  if (latitude.value !== null && longitude.value !== null) {
-    map.value = null;
-    createMap();
-  }
-});
-
 const createMap = () => {
-  console.log(latitude.value, longitude.value);
-  if (latitude.value !== null && longitude.value !== null) {
-    map.value = new ol.Map({
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.XYZ({
-            url: `https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=00BVJaSkhTUVFkQcPsvf`,
-          }),
+  map.value = new ol.Map({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.XYZ({
+          url: `https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=00BVJaSkhTUVFkQcPsvf`,
         }),
-      ],
-      target: "map",
-      view: new ol.View({
-        center: ol.proj.fromLonLat([
-          Number(longitude.value),
-          Number(latitude.value),
-        ]),
-        zoom: 5,
       }),
-    });
+    ],
+    target: "map",
+  });
 
-    if (latitude.value !== null && longitude.value !== null) {
-      const marker = new ol.layer.Vector({
-        source: new ol.source.Vector({
-          features: [
-            new ol.Feature({
-              geometry: new ol.geom.Point(
-                ol.proj.fromLonLat([
-                  Number(longitude.value),
-                  Number(latitude.value),
-                ])
-              ),
-            }),
-          ],
-        }),
-        style: new ol.style.Style({
-          image: new ol.style.Icon({
-            src: "https://docs.maptiler.com/openlayers/default-marker/marker-icon.png",
-            anchor: [0.5, 1],
-          }),
-        }),
-      });
+  // Create the marker layer
+  markerLayer.value = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      features: [],
+    }),
+    style: new ol.style.Style({
+      image: new ol.style.Icon({
+        src: "https://docs.maptiler.com/openlayers/default-marker/marker-icon.png",
+        anchor: [0.5, 1],
+      }),
+    }),
+  });
 
-      map.value.addLayer(marker);
-    }
+  // Set a name for the marker layer for easy identification
+  markerLayer.value.set("name", "marker");
+
+  // Add the marker layer to the map
+  map.value.addLayer(markerLayer.value);
+};
+
+const updateMap = () => {
+  if (map.value && latitude.value !== null && longitude.value !== null) {
+    // Update the map's view (center and zoom) with the new coordinates
+    const view = map.value.getView();
+    view.setCenter(
+      ol.proj.fromLonLat([Number(longitude.value), Number(latitude.value)])
+    );
+    view.setZoom(5);
+
+    // Update the marker layer with the new coordinates
+    const markerSource = markerLayer.value.getSource();
+    markerSource.clear();
+    markerSource.addFeature(
+      new ol.Feature({
+        geometry: new ol.geom.Point(
+          ol.proj.fromLonLat([Number(longitude.value), Number(latitude.value)])
+        ),
+      })
+    );
   }
 };
 </script>
