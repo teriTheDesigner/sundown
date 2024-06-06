1ea26186bf4e0e9458dab7507a7da5cca8a01cae
teriTheDesigner <tere1109@stud.kea.dk>
2024-06-06 14:00:16 +0200

    installing the library

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,16 @@ export default defineNuxtConfig({
   },
 
   devtools: { enabled: false },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
 });
