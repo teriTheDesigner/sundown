497a721f3347ce552f3b36be27e94bb597ad40a3
teriTheDesigner <tere1109@stud.kea.dk>
2024-06-09 13:30:40 +0200

    fix: adding functionality to new components

--- pages/index.vue
+++ pages/index.vue
@@ -52,7 +52,7 @@ function Login() {
 
   if (foundUser) {
     localStorage.setItem("validUser", email.value);
-    console.log("user logged in");
+
     globalStore.setEmail(email.value);
     globalStore.setUserName(foundUser.username);
     globalStore.setCodeName(foundUser.code_name);
@@ -62,7 +62,6 @@ function Login() {
     useRouter().push("/dashboard");
   } else {
     userNotFound.value = true;
-    console.log("email not found", userNotFound.value);
   }
 }
 
@@ -73,77 +72,6 @@ const isButtonDisabled = computed(() => {
 });
 </script>
 <template>
-  <!-- <div
-    class="h-screen backgroundImg text-white flex flex-col place-items-center"
-  >
-    <div class="content-container flex flex-col place-items-center m-auto">
-      <form
-        @submit.prevent="Login"
-        class="flex rounded-xl flex-col bg-white text-black p-6 items-center justify-between m-auto w-80 h-96"
-      >
-        <div class="p-6">
-          <p v-if="!userNotFound">LOGIN</p>
-          <p class="text-xs text-red-600" v-if="userNotFound">
-            Please enter a correct email and password
-          </p>
-        </div>
-
-        <label class="flex flex-col h-1/3 gap-2">
-          E-mail:
-          <input
-            class="border p-2 w-56 focus:outline-gray-600 border-gray-600"
-            v-model="email"
-            v-on:blur="(e) => emailBlur(e.target.value)"
-            v-on:focus="emailFocus"
-            type="email"
-            required
-          />
-          <span class="text-xs text-red-600" v-if="emailError">{{
-            emailError
-          }}</span>
-        </label>
-        <label class="flex flex-col h-1/3 gap-2">
-          Password:
-          <input
-            class="border p-2 w-56 border-gray-600 focus:outline-gray-600"
-            v-on:blur="(e) => passwordBlur(e.target.value)"
-            v-on:focus="passwordFocus"
-            v-model="password"
-            type="password"
-            required
-          />
-          <span class="text-xs text-red-600" v-if="passwordError">{{
-            passwordError
-          }}</span>
-        </label>
-        <button
-          type="submit"
-          :disabled="isButtonDisabled"
-          :class="{
-            'bg-black': !passwordError && !emailError,
-            'opacity-60': isButtonDisabled,
-          }"
-          class="hover:scale-105 rounded h-12 w-56 bg-black text-white"
-        >
-          LOGIN
-        </button>
-      </form>
-    </div>
-   <div
-      v-if="userNotFound"
-      class="fixed inset-0 flex modal bg-black bg-opacity-50 items-center justify-center"
-    >
-      <div class="modal bg-white p-12 rounded-xl text-black">
-        <p>Email not found.</p>
-        <button
-          @click="userNotFound = false"
-          class="mt-4 px-4 py-2 bg-black text-white rounded"
-        >
-          Close
-        </button>
-      </div>
-    </div> 
-  </div> -->
   <div class="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
     <form
       @submit.prevent="Login"
@@ -154,34 +82,57 @@ const isButtonDisabled = computed(() => {
       >
         <div class="grid gap-2">
           <h1 class="text-3xl font-bold mb-2">Login</h1>
-          <p class="text-gray-300 text-sm mb-2">
+          <p class="text-gray-300 text-sm mb-2" v-if="!userNotFound">
             Enter your email below to login to your account
+          </p>
+          <p class="text-sm text-red-400 mb-2" v-if="userNotFound">
+            Please enter a valid email and password
           </p>
         </div>
         <div class="grid gap-6">
           <div class="grid gap-2">
             <Label for="email">Email</Label>
             <Input
+              v-model="email"
+              v-on:blur="(e) => emailBlur(e.target.value)"
+              v-on:focus="emailFocus"
               id="email"
               type="email"
               placeholder="m@example.com"
               required
               class="bg-black border-gray-500"
             />
+            <span class="text-xs text-red-400" v-if="emailError">{{
+              emailError
+            }}</span>
           </div>
           <div class="grid gap-2">
             <div class="flex items-center">
               <Label for="password">Password</Label>
             </div>
             <Input
+              v-on:blur="(e) => passwordBlur(e.target.value)"
+              v-on:focus="passwordFocus"
+              v-model="password"
               id="password"
               class="bg-black border-gray-500"
               type="password"
               placeholder="password"
               required
             />
+            <span class="text-xs text-red-400" v-if="passwordError">{{
+              passwordError
+            }}</span>
           </div>
-          <Button type="submit" class="w-full bg-white text-black">
+          <Button
+            type="submit"
+            :disabled="isButtonDisabled"
+            :class="{
+              'bg-white': !passwordError && !emailError,
+              'opacity-80': isButtonDisabled,
+            }"
+            class="w-full bg-white text-black hover:bg-white hover:text-black"
+          >
             Login
           </Button>
         </div>
@@ -196,3 +147,20 @@ const isButtonDisabled = computed(() => {
     </div>
   </div>
 </template>
+<style scoped>
+/* Ensure the input field retains its dark background */
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+input:-webkit-autofill:active {
+  transition: background-color 5000s ease-in-out 0s;
+  -webkit-text-fill-color: white !important;
+  background-color: black !important;
+}
+
+/* Adjust the autofill text color */
+input:-internal-autofill-selected {
+  background-color: black !important;
+  color: white !important;
+}
+</style>
