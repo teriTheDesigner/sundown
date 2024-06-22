9567622cdca2237c27b2da55cbd02a4f98f86720
teriTheDesigner <tere1109@stud.kea.dk>
2024-06-22 20:16:22 +0200

    fix width on 1st step

--- pages/step1.vue
+++ pages/step1.vue
@@ -32,61 +32,63 @@ onMounted(() => {
 
 <template>
   <div class="ml-10 mr-10" v-if="displayPage">
-    <Nav></Nav>
-    <Stepper />
-    <div class="h-screen pt-20 pb-20">
-      <form
-        @submit.prevent="updateUser"
-        class="content-container mx-auto grid grid-cols-12 justify-center"
-      >
-        <div class="col-start-1 col-end-6 flex flex-col gap-20">
-          <label class="flex flex-col gap-4">
-            Mission Name
-            <input
-              v-model="reportName"
-              class="border text-black p-2 h-12 focus:outline-gray-600 border-gray-600"
-              type="text"
-              required
-            />
-          </label>
-          <label class="flex flex-col gap-4">
-            Mission Description
-            <textarea
-              v-model="reportDescription"
-              class="border focus:outline-gray-600 text-black p-2 border-gray-600 h-60"
-              type="text"
-              required
-            ></textarea>
-          </label>
-        </div>
-        <div class="col-start-9 col-end-13 flex flex-col justify-between">
-          <label class="flex flex-col gap-4">
-            Mission Date
-            <input
-              v-model="reportDate"
-              class="border focus:outline-gray-600 text-black p-2 border-gray-600 h-12"
-              type="date"
-              required
-            />
-          </label>
-          <div class="flex flex-col gap-4">
-            <p v-if="isButtonDisabled" class="text-sm text-red-500">
-              <b> Please fill in all fields.</b>
-            </p>
-            <button
-              :disabled="isButtonDisabled"
-              type="submit"
-              :class="{
-                'hover:scale-105': !isButtonDisabled,
-                'opacity-60': isButtonDisabled,
-              }"
-              class="rounded h-12 w-52 bg-white text-black flex flex-col items-center justify-center"
-            >
-              NEXT STEP
-            </button>
+    <div class="max-w-screen-2xl mx-auto">
+      <Nav></Nav>
+      <Stepper />
+      <div class="h-screen pt-20 pb-20">
+        <form
+          @submit.prevent="updateUser"
+          class="content-container mx-auto grid grid-cols-12 justify-center"
+        >
+          <div class="col-start-1 col-end-6 flex flex-col gap-20">
+            <label class="flex flex-col gap-4">
+              Mission Name
+              <input
+                v-model="reportName"
+                class="border text-black p-2 h-12 focus:outline-gray-600 border-gray-600"
+                type="text"
+                required
+              />
+            </label>
+            <label class="flex flex-col gap-4">
+              Mission Description
+              <textarea
+                v-model="reportDescription"
+                class="border focus:outline-gray-600 text-black p-2 border-gray-600 h-60"
+                type="text"
+                required
+              ></textarea>
+            </label>
           </div>
-        </div>
-      </form>
+          <div class="col-start-9 col-end-13 flex flex-col justify-between">
+            <label class="flex flex-col gap-4">
+              Mission Date
+              <input
+                v-model="reportDate"
+                class="border focus:outline-gray-600 text-black p-2 border-gray-600 h-12"
+                type="date"
+                required
+              />
+            </label>
+            <div class="flex flex-col gap-4">
+              <p v-if="isButtonDisabled" class="text-sm text-red-500">
+                <b> Please fill in all fields.</b>
+              </p>
+              <button
+                :disabled="isButtonDisabled"
+                type="submit"
+                :class="{
+                  'hover:scale-105': !isButtonDisabled,
+                  'opacity-60': isButtonDisabled,
+                }"
+                class="rounded h-12 w-52 bg-white text-black flex flex-col items-center justify-center"
+              >
+                NEXT STEP
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
   <div v-else class="h-screen flex place-items-center">
